181a7ba4062e14b487227d4f138fb9803dfb6f55
yixin <lc4@lc4s-iMac.local>
2017-12-08 08:08:39 +1300

    add drawing curves statements

--- TestPython/Jupyter.py
+++ TestPython/Jupyter.py
@@ -1,4 +1,51 @@
 
+#############pie chart######################
+
+"""
+import matplotlib.pyplot as plt
+labels='frogs','hogs','dogs','logs'
+sizes=15,20,45,10
+colors='yellowgreen','gold','lightskyblue','lightcoral'
+explode=0,0.1,0,0
+plt.pie(sizes,explode=explode,labels=labels,colors=colors,autopct='%1.1f%%',shadow=True,startangle=50)
+plt.axis('equal')
+plt.show()
+"""
+##################################################
+
+%matplotlib inline
+import numpy as np
+import matplotlib.pyplot as plt
+from pylab import *
+
+
+income = [3.2 4.1 5.0 5.6];
+outgo = [2.5 4.0 3.35 4.9];
+
+subplot(2,1,1); plot(income)
+subplot(2,1,2); plot(outgo)
+
+
+
+x = np.arange(-5.0, 5.0, 0.02)
+y1 = np.sin(x)
+
+plt.figure(1)
+plt.subplot(211)
+plt.plot(x, y1)
+
+plt.subplot(212)
+#设置x轴范围
+xlim(-2.5, 2.5)
+#设置y轴范围
+ylim(-1, 1)
+plt.plot(x, y1)
+
+
+
+
+
+
 # coding: utf-8
 
 # In[1]:
--- TestPython/predict_house_price.py
+++ TestPython/predict_house_price.py
@@ -5,7 +5,7 @@ Created on Thu Dec  7 13:48:49 2017
 
 @author: lc4
 """
-
+"""
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -39,4 +39,45 @@ predictvalue = 7
 result = linear_model_main(X,Y,predictvalue)
 print ("Intercept value " , result['intercept'])
 print ("coefficient" , result['coefficient'])
-print ("Predicted value: ",result['predicted_value'])/
+print ("Predicted value: ",result['predicted_value'])
+"""
+import numpy as np
+
+
+#%matplotlib inline
+
+##############no work############
+"""
+import matplotlib.pyplot as plt
+from pylab import *
+
+
+income = [3 4 5 5];
+outgo = [2.5 4.0 3.35 4.9];
+
+subplot(2,1,1); plot(income)
+subplot(2,1,2); plot(outgo)
+"""
+####################################
+
+
+############draw a sin curve#############
+"""
+x = np.arange(-5.0, 5.0, 0.02)
+y1 = np.sin(x)
+
+plt.figure(1)
+
+#subplot is a matlab function
+#(211) means 2 lines multiply 1 column, and the position is 1
+plt.subplot(211)
+plt.plot(x, y1)
+
+plt.subplot(212)
+#设置x轴范围
+xlim(-2.5, 2.5)
+#设置y轴范围
+ylim(-1, 1)
+plt.plot(x, y1)
+"""
+#############################################
