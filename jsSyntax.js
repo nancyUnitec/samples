7fe9b1e1ab30fbf863fb18bd8ffbfc0321e73eb5
nancyUnitec <xin080124@hotmail.com>
2018-02-11 20:53:21 +1300

    add the use of eval() method

--- jsSyntax.js
+++ jsSyntax.js
@@ -17,3 +17,26 @@ materials.map((material) => {
 
 //shorter
 materials.map(material => material.length); // [8, 6, 7, 9]
+
+//the use of eval()
+var evalString="testEval()"
+
+function testEval(){
+	alert("testEval!");
+}
+
+function testjs()
+{
+  eval(evalString); //will call testEval. eval is  used to create js dynamic code 
+  //like function pointer to some extent
+}
+
+//document.getElementById("").attr
+function forLoop(){
+  for (var loop = 1; loop < 10; loop++)
+	{
+		// eval('document.getElementById("div"+loop).innerHTML="123"');
+		document.getElementById("div"+loop).innerHTML="123"; //will show "123" in div1,div2 and div3
+	}
+}
+	
