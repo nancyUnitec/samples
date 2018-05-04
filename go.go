249f067c07f6f10e83ef666fb88f5e2667451ea1
nancy <xin080124@hotmail.com>
2018-05-04 22:35:59 +1200

    add ack() and nack()

--- go.go
+++ go.go
@@ -1,3 +1,9 @@
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 //require contents from mac in company.
 
 //log
@@ -7,42 +13,52 @@
 //marshal and unmarshal
 
 //struct to json
-func testMarshal(){
-    fmt.Printf ( "mmmmmmmmmmmmmmmmmmmarshal \n" ) 
-    type ColorGroup struct { 
-        ID     int 
-        Name   string 
-        Colors [ ] string 
-    } 
-    group := ColorGroup { 
-        ID :     1 , 
-        Name :   "Reds" , 
-        Colors : [ ] string { "Crimson" , "Red" , "Ruby" , "Maroon" } , 
-    } 
-    b , err := json. Marshal ( group ) 
-    if err != nil { 
-        fmt. Println ( "error:" , err ) 
-    } 
-    os. Stdout . Write ( b ) 
-    //output is a json format: {"ID":1,"Name":"Reds","Colors":["Crimson","Red","Ruby","Maroon"]}
+func testMarshal() {
+	fmt.Printf("mmmmmmmmmmmmmmmmmmmarshal \n")
+	type ColorGroup struct {
+		ID     int
+		Name   string
+		Colors []string
+	}
+	group := ColorGroup{
+		ID:     1,
+		Name:   "Reds",
+		Colors: []string{"Crimson", "Red", "Ruby", "Maroon"},
+	}
+	b, err := json.Marshal(group)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	os.Stdout.Write(b)
+	//output is a json format: {"ID":1,"Name":"Reds","Colors":["Crimson","Red","Ruby","Maroon"]}
 }
 
 //json to struct
-func testUnMarshal(){
-    fmt.Printf ( "\n uuuuuuuuuuuuunmarshal \n" ) 
-    var jsonBlob = [ ] byte ( ` [ 
+func testUnMarshal() {
+	fmt.Printf("\n uuuuuuuuuuuuunmarshal \n")
+	var jsonBlob = []byte(` [ 
         { "Name" : "Platypus" , "Order" : "Monotremata" } , 
         { "Name" : "Quoll" ,     "Order" : "Dasyuromorphia" } 
-    ] ` ) 
-    type Animal struct { 
-        Name  string 
-        Order string 
-    } 
-    var animals [ ] Animal 
-    err := json. Unmarshal ( jsonBlob , & animals ) 
-    if err != nil { 
-        fmt. Println ( "error:" , err ) 
-    } 
-    fmt.Printf ( "%+v" , animals ) 
-    //output: [{Name:Platypus Order:Monotremata} {Name:Quoll Order:Dasyuromorphia}]
+    ] `)
+	type Animal struct {
+		Name  string
+		Order string
+	}
+	var animals []Animal
+	err := json.Unmarshal(jsonBlob, &animals)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Printf("%+v", animals)
+	//output: [{Name:Platypus Order:Monotremata} {Name:Quoll Order:Dasyuromorphia}]
 }
+
+//pubsub messages
+//not to publist the message, use
+msg.Ack()
+
+//resend the msg immediately
+msg.Nack()
+
+//resend the msg every 60 secs
+do nothing, return directly 
